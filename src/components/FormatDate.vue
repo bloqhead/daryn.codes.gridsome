582898a804cab5cb49518f511689a6ee7d35d153
Daryn St. Pierre <daryniscool@gmail.com>
2020-05-12 23:08:26 -0400

    fixed the date handling for the blog.

--- src/components/FormatDate.vue
+++ src/components/FormatDate.vue
@@ -1,29 +1,34 @@
 <template>
   <div>
-    <fa :icon="[ 'fa', 'clock' ]" />&nbsp;
-    <time :datetime="date" v-html="readableDate(date)" />
+    <fa :icon="['fa', 'clock']" />
+    &nbsp;
+    <time :datetime="date">
+      {{ niceDate }}
+    </time>
   </div>
 </template>
 
 <script>
+import dayjs from "dayjs";
+
 export default {
   props: {
     date: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
-  methods: {
-    readableDate(value) {
-      let newDate = new Date(value).toLocaleDateString("en-US", {
-        year: "numeric",
-        month: "long",
-        day: "numeric"
-      });
+  computed: {
+    niceDate() {
+      const rawDate = new Date(this.date);
+      const fixedDate = new Date(
+        rawDate.getTime() + rawDate.getTimezoneOffset() * 60000
+      ).toISOString();
+
+      const newDate = dayjs(fixedDate).format("MMM DD, YYYY");
 
       return newDate;
-    }
-  }
+    },
+  },
 };
 </script>
-
