8baf075be009b6042414d872bfc05bc6a87ac0d4
Daryn St. Pierre <daryn.codes@protonmail.com>
2021-06-01 11:41:39 -0400

    updating email address

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -6,7 +6,7 @@
       <p>
         If you want to drop me a line to ask a question or just say hi, you can
         email me at
-        <a href="mailto:daryn@hey.com">daryn@hey.com</a>. I'll try to respond in
+        <a href="mailto:daryn.codes@pm.me">daryn.codes@pm.me</a>. I'll try to respond in
         a day or two.
       </p>
     </div>
